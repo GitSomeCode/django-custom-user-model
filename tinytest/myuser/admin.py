03ae373aa13aeaad1097ad3ba144e4b72e9596bb
gitsomecode <sjbitcode@gmail.com>
2016-06-11 18:00:51 -0400

    Add UserAdmin class to admin.py.

--- tinytest/myuser/admin.py
+++ tinytest/myuser/admin.py
@@ -7,6 +7,7 @@ from django.utils.translation import ugettext_lazy as _
 
 from .models import User
 
+
 class UserCreationForm(forms.ModelForm):
     """
     A form for creating new users, with no privileges,
@@ -73,3 +74,38 @@ class UserChangeForm(forms.ModelForm):
         field does not have access to the initial value
         """
         return self.initial['password']
+
+
+class UserAdmin(BaseUserAdmin):
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference the removed 'username' field
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('date_joined',)})
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide'),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
+    # The forms to add and change user instances
+    form = UserCreationForm
+    add_form = UserChangeForm
+
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+    admin.site.register(User, UserAdmin)
+    admin.site.unregister(Group)
+
+
